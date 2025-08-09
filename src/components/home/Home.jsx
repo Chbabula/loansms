7f715100030343f1368b519e7971027fc0994ce2
Ch_babula <chinthanippulababulal@gmail.com>
2025-08-09 13:23:58 +0530

    connction

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,11 +1,9 @@
-
-import styles from './home.module.css';
+import styles from "./home.module.css";
 
 const Home = () => {
-
-    const handleApplyClick = () => {
-    const form = document.getElementById('contact');
-    if (form) form.scrollIntoView({ behavior: 'smooth' });
+  const handleApplyClick = () => {
+    const form = document.getElementById("contact");
+    if (form) form.scrollIntoView({ behavior: "smooth" });
   };
 
   return (
@@ -16,23 +14,36 @@ const Home = () => {
 
       <div className={styles.homeContent}>
         <div className={styles.textSection}>
-            <h2 className={styles.mainText}>Fast, Flexible, and Affordable Loans – When You Need Them Most</h2>
-          <p>At MS Loans, we make borrowing simple. Whether you're looking to finance a big purchase, consolidate debt, cover unexpected expenses, or grow your business, we offer personalized loan solutions designed to fit your needs and budget.</p>
+          <h2 className={styles.mainText}>
+            Fast, Flexible, and Affordable Loans – When You Need Them Most
+          </h2>
+          <p>
+            At MS Loans, we make borrowing simple. Whether you're looking to
+            finance a big purchase, consolidate debt, cover unexpected expenses,
+            or grow your business, we offer personalized loan solutions designed
+            to fit your needs and budget.
+          </p>
           <h4>✅ Quick Approvals</h4>
-          <p>Apply online in minutes and get a decision fast – no long lines or endless paperwork.</p>
+          <p>
+            Apply online in minutes and get a decision fast – no long lines or
+            endless paperwork.
+          </p>
           <h4>✅ Flexible Terms</h4>
-          <p>Choose repayment plans that work for you. We offer competitive rates and clear terms with no hidden fees.</p>
+          <p>
+            Choose repayment plans that work for you. We offer competitive rates
+            and clear terms with no hidden fees.
+          </p>
           <h3>Ready to Get Started?</h3>
-          <p>Apply now and take the first step toward achieving your financial goals.</p>
+          <p>
+            Apply now and take the first step toward achieving your financial
+            goals.
+          </p>
           <p>Fast and easy loans in just a few clicks.</p>
           <div className={styles.applySection}>
             <button onClick={handleApplyClick}>Apply Now</button>
           </div>
         </div>
-
-        
       </div>
-    
     </section>
   );
 };
